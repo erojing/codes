e803711b93e5870daa49c46534541a1cb059321c
erojing <asd789357@gmail.com>
2022-01-08 13:18:49 +0800

    2022/1/8 uploaded

---
+++ RA/科技部計畫/util.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+# draw coefficients plot
+def plot_coefficients(coefs, n_highlight):
+    _, ax = plt.subplots(figsize=(9, 6))
+    n_features = coefs.shape[0]
+    alphas = coefs.columns
+    for row in coefs.itertuples():
+        ax.semilogx(alphas, row[1:], ".-", label=row.Index)
+
+    alpha_min = alphas.min()
+    top_coefs = coefs.loc[:, alpha_min].map(abs).sort_values().tail(n_highlight)
+    for name in top_coefs.index:
+        coef = coefs.loc[name, alpha_min]
+        plt.text(
+            alpha_min, coef, name + "   ",
+            horizontalalignment="right",
+            verticalalignment="center"
+        )
+
+    ax.yaxis.set_label_position("right")
+    ax.yaxis.tick_right()
+    ax.grid(True)
+    ax.set_xlabel("alpha")
+    ax.set_ylabel("coefficient")
